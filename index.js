2a4d1f94f09751e69d284c8a3b6f241eca4b195a
Kamalraj David Chitturi <99992423+techwithkamalraj@users.noreply.github.com>
2024-05-01 19:25:10 +0530

    "second commit"

--- index.js
+++ index.js
@@ -1,50 +1,61 @@
-
-
-
 let password = document.getElementById('btn');
-function gg(e){
-        e.preventDefault();
-        let length = document.getElementById('len').value;
-        let p = document.getElementById('length');
-        p.innerText = length;
-    
-        let lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
-        let upperCaseLetters = lowercaseLetters.toUpperCase(lowercaseLetters);
-        let numbers = '0123456789'
-        let symbols = '@#$%^&*';
-        
-        let char = "";
-        
-        if(document.getElementById('upper').checked){
-           char += upperCaseLetters;
-        }
-        if(document.getElementById('lower').checked){
-           char += lowercaseLetters;
-        }
-        if(document.getElementById('Number').checked){
-           char += numbers;
-        }
-        if(document.getElementById('symbol').checked){
-           char += symbols;
-        }
-    
-        let password = "";
-        
-    
-        for(let i = 0;i < length;i++){
-            const randomIndex = Math.floor(Math.random() * char.length);
-            console.log(randomIndex)
-            password += char[randomIndex]
-        }
-    
-        document.getElementById('password').innerText = password;
-    
-    }
-    // let length = document.getElementById('length').value;
+let rangeInput = document.getElementById('len');
+let p = document.getElementById('length');
+
+// Display initial value
+p.innerText = rangeInput.value;
+
+function updateLength() {
+    // Update value of p when range input changes
+    p.innerText = rangeInput.value;
+}
+// Get all elements with class "b"
+
+
+function generatePassword() {
    
+    let length = rangeInput.value;
 
-password.addEventListener('click',gg)
+    let lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+    let upperCaseLetters = lowercaseLetters.toUpperCase();
+    let numbers = '0123456789';
+    let symbols = '@#$%^&*()`"?><';
+    
+    
+  
+ 
 
+    let char = "";
+    
+    if(document.getElementById('upper').checked){
+        char += upperCaseLetters;
+    }
+    if(document.getElementById('lower').checked){
+        char += lowercaseLetters;
+    }
+    if(document.getElementById('Number').checked){
+        char += numbers;
+    }
+    if(document.getElementById('symbol').checked){
+        char += symbols;
+    }
 
+    if(char === "") {
+        // If no character set is selected, display a message
+        document.getElementById('password').innerText = "Select any one Character Set";
+        return;
+    }
 
+    let password = "";
+    
+    for(let i = 0; i < length; i++){
+        const randomIndex = Math.floor(Math.random() * char.length);
+        password += char[randomIndex];
+    }
 
+    document.getElementById('password').innerText = password;
+}
+
+// Add event listeners
+password.addEventListener('click', generatePassword);
+rangeInput.addEventListener('input', updateLength);
